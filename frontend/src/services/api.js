834ed05071f1b157848fc3d014dc90765aef794e
Bravine Mulunda <bravinemulunda7@gmail.com>
2026-09-16 12:45:12 +0300

    Connect frontend to online backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,25 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-
-  baseURL: "http://172.20.10.11:5000/api",
-
+  baseURL: "https://ushindi-management-system.onrender.com/api",
 });
 
 // Automatically attach the token to every request
-
 API.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
   if (token) {
-
     config.headers.Authorization = `Bearer ${token}`;
-
   }
 
   return config;
-
 });
 
 export default API;
